2e8e8fd3c908afac228865a834898d3637d933c5
Hunter Dyar <bloop@hdyar.com>
2019-12-03 16:40:25 -0500

    Added Attributes

--- Assets/Scripts/CharacterControllers/Asteroids/AsteroidsCharacterController.cs
+++ Assets/Scripts/CharacterControllers/Asteroids/AsteroidsCharacterController.cs
@@ -20,18 +20,30 @@ namespace Blooper{
     //ANyway just fiddle a bunch. The example scene isn't really a starting point, i didn't spend much time with that.
     public class AsteroidsCharacterController : MonoBehaviour
     {
+        [Header("Character Settings")]
+        [Tooltip("Controls how quickly the character can rotate")]
         public float rotationForce;
+        [Tooltip("Controls movement acceleration")]
         public float movementForce;
+        [Tooltip("When false player will not be able to throttle in opposite direction they are facing.")]
         public bool allowBackwardsMovement = true;
-        public float damageTaken = 0;
+
+        [HideInInspector]//Even though the variable is public, it will not appear as editable in the inspector.
+        public float damageTaken = 0;//We aren't actually using this code, but perhaps I want to make it public so other scripts can how much damage this one has taken?
+        //okay, fine, it would probably be private but here is an example of "HideInInspector". It helps keep things tidy.
         Rigidbody2D rb;
         
         void Awake(){
-            rb = GetComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody2D>();//This should be familiar by now.
         }
         void Start()
         {
             damageTaken = 0;
+
+            //The following are examples of some tests of likely mistakes when we are setting up the controller.
+
+            //If you are worried about the performance impact of tests like this, there are ways to make sections of your code not even compile in finished builds! Neat!
+            //We also don't need to do it on Start, we could use one of the event functions for the editor, and these could get called without us even entering play mode.
             if(rotationForce == 0){
                 Debug.LogWarning(gameObject.name+" rotation force is 0. They can't rotate!",this);
             }
@@ -39,7 +51,7 @@ namespace Blooper{
                 Debug.LogWarning(gameObject.name+" movementForce is 0! They can't move.",this);
             }
             if(rb.gravityScale != 0){
-                Debug.LogWarning(gameObject.name + " has gravity. You probably forgot to go into the rigibody and appropriate adjustments.");
+                Debug.LogWarning(gameObject.name + " has gravity. You should probably go into the rigibody and make appropriate adjustments.");
             }
         }
 
@@ -62,7 +74,7 @@ namespace Blooper{
             Vector2 forceVector = transform.up*throttleInput;//Our forward direction is where we want to go.
             rb.AddForce(forceVector,ForceMode2D.Force);
         }
-        public void OnCollisionEnter2D(Collision2D collision){
+        public void OnCollisionEnter2D(Collision2D collision){//dont forget we are usng Physics2D, not Physics!
             
             damageTaken = damageTaken + collision.relativeVelocity.magnitude;
             
