41af6cb7144886ea98b3ee047f0347626a66a90a
Hunter Dyar <bloop@hdyar.com>
2019-12-03 11:37:28 -0500

    Took sampleInput scripts out of the namespace
    In the real world a script like this may or may not be in such a namespace, depending on how we wanted to set things up. i want to demonstrate how namespaces work.

--- Assets/Scripts/CharacterControllers/Asteroids/AsteroidsCharacterInput.cs
+++ Assets/Scripts/CharacterControllers/Asteroids/AsteroidsCharacterInput.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Blooper{
-    public class AsteroidsCharacterInput : MonoBehaviour
+using Blooper;
+public class AsteroidsCharacterInput : MonoBehaviour
+{
+    AsteroidsCharacterController controller;//"using blooper" up top? That lets us reference this thing from another namespace.
+    //We could also do "Blooper.AsteroidsCharacterController controller;"
+    void Awake()
     {
-        AsteroidsCharacterController controller;
-        void Awake()
-        {
-            controller = GetComponent<AsteroidsCharacterController>();    
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            controller.Rotate(Input.GetAxis("Horizontal"));
-            controller.Throttle(Input.GetAxis("Vertical"));
-        }
+        controller = GetComponent<AsteroidsCharacterController>();    
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Rotate(Input.GetAxis("Horizontal"));
+        controller.Throttle(Input.GetAxis("Vertical"));
+    }
 }
+
