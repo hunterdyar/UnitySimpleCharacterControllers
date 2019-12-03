9935dac79d9ae15f1e19a2ffb0eed598555a1fa5
Hunter Dyar <bloop@hdyar.com>
2019-12-03 11:31:33 -0500

    Asteroids Movement Added
    Simple asteroids-style character controller.

---
+++ Assets/Scripts/CharacterControllers/Asteroids/AsteroidsCharacterInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Blooper{
+    public class AsteroidsCharacterInput : MonoBehaviour
+    {
+        AsteroidsCharacterController controller;
+        void Awake()
+        {
+            controller = GetComponent<AsteroidsCharacterController>();    
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            controller.Rotate(Input.GetAxis("Horizontal"));
+            controller.Throttle(Input.GetAxis("Vertical"));
+        }
+    }
+
+}
